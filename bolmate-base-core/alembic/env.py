eec4a6783084baf9319dc1fc1fe9d5af66560c7f
Wojciech <102759591+Wojciech512@users.noreply.github.com>
2025-12-02 00:15:22 +0100

    Use default database URL for Alembic

--- bolmate-base-core/alembic/env.py
+++ bolmate-base-core/alembic/env.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 from sqlalchemy import engine_from_config, pool
 from alembic import context
+from config.settings import DEFAULT_DATABASE_URL
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
@@ -20,9 +21,8 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-database_url = os.getenv("DATABASE_URL")
-if database_url:
-    config.set_main_option("sqlalchemy.url", database_url)
+database_url = os.getenv("DATABASE_URL", DEFAULT_DATABASE_URL)
+config.set_main_option("sqlalchemy.url", database_url)
 
 target_metadata = Base.metadata
 
